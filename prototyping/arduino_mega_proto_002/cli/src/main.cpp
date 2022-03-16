8ce56da328a66726363ed493dce4919f32d82f56
Walid Amriou <walidamriou@gmail.com>
2022-03-16 08:37:39 +0100

    Update main.cpp

--- prototyping/arduino_mega_proto_002/cli/src/main.cpp
+++ prototyping/arduino_mega_proto_002/cli/src/main.cpp
@@ -1,16 +1,14 @@
 #include <Arduino.h>
-#include "ecli/ecli.h"
+#include "embeddedcli/embeddedcli.h"
 
 void setup(){
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(ECLI_LED, OUTPUT);
   
   // init
-  ecli_init();
+  EmbeddedCLI_Init();
 }
 
 void loop(){
-  ECLI_loop();
+  EmbeddedCLI_Loop();
 }
 
