fa6eeed5d229bb03ebf5aad5610156144586a9b6
Walid Amriou <walidamriou@gmail.com>
2022-03-16 08:37:35 +0100

    Create embeddedcli_config.h

---
+++ prototyping/arduino_mega_proto_002/cli/src/embeddedcli/embeddedcli_config.h
@@ -0,0 +1,24 @@
+/**
+ * @file embeddedcli_config.h
+ * @brief header file to config Embedded CLI library
+ * 
+ * @author Walid Amriou (walidamriou)
+ * @date 15/03/2022
+ * 
+ * Embedded CLI
+ * Open and Lightweight Command Line Interface for Embedded Systems
+ * 
+ * docs: https://EmbeddedCLI.walidamriou.com
+ * source: https://github.com/walidamriou/EmbeddedCLI
+ * 
+ */
+
+#ifndef EMBEDDEDCLI_CONFIG_H 
+#define EMBEDDEDCLI_CONFIG_H
+
+#define EMBEDDEDCLI_UART_BUS_SPEED     9600
+#define EMBEDDEDCLI_LED                13    // GPIO connected to led to debug ECLI
+#define DEVICE_NAME                    "Embedded system device test"
+#define DEVICE_FIRMWARE_VERSION        "v1.0.0"
+
+#endif
