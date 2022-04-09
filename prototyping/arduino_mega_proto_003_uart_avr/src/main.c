2e6bd59c138480c3c106e6e8568f4fa29ee4fabf
Walid Amriou <walidamriou@gmail.com>
2022-04-10 00:53:45 +0100

    Update main.c

--- prototyping/arduino_mega_proto_003_uart_avr/src/main.c
+++ prototyping/arduino_mega_proto_003_uart_avr/src/main.c
@@ -5,17 +5,9 @@ int main(void) {
   
   embeddedcli_init();
   embeddedcli_cmd_help();
-  //char c[] = "hello google dodo ";
-  //uint8_t c  = 104;
-  //uint8_t c[]={104,113,107,101,120};
-  //hal_serial_UART0_send((uint8_t *)&c,sizeof(c));
 
-  // Enable interrupts
-	sei();
-  while (1) {
-
-  } 
-  return (0); 
+  while (1) {} 
+  return 0; 
 }
 
 
