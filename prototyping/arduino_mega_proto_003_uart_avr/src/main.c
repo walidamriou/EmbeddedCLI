265fd768f780f999f40eea03ef6fb32297d001ee
Walid Amriou <walidamriou@gmail.com>
2022-04-11 13:20:59 +0100

    Update main.c

--- prototyping/arduino_mega_proto_003_uart_avr/src/main.c
+++ prototyping/arduino_mega_proto_003_uart_avr/src/main.c
@@ -1,9 +1,15 @@
 #include "atmelhal/hal_sources.h"
+#include "embeddedcli/embeddedcli_user.h"
 #include "embeddedcli/embeddedcli.h"
 
+
 int main(void) {
-  
+
+  //char note[] = "\ndata";
   embeddedcli_init();
+  //data((uint8_t *)&note,sizeof(note));
+  (*func_ptr[0])();
+  hal_serial_UART0_send((uint8_t *)cmd_list[0],4);
   embeddedcli_cmd_help();
 
   while (1) {} 
