6379dcbf20500bafbc91f82828a14991e5374e20
Walid Amriou <walidamriou@gmail.com>
2022-04-11 21:24:15 +0100

    add prototype for arduino uno 3 cmd

---
+++ prototyping/arduino_uno_proto_005_uart_avr /src/main.c
@@ -0,0 +1,16 @@
+#include "atmelhal/hal_sources.h"
+#include "embeddedcli/embeddedcli_user.h"
+#include "embeddedcli/embeddedcli.h"
+
+
+int main(void) {
+
+  embeddedcli_init();
+
+  while (1) {} 
+  return 0; 
+}
+
+
+
+
