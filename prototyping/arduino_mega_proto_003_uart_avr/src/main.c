250a7b2af0b698c2d47b8655348803acf374cd86
Walid Amriou <walidamriou@gmail.com>
2022-04-11 13:36:03 +0100

    Update main.c

--- prototyping/arduino_mega_proto_003_uart_avr/src/main.c
+++ prototyping/arduino_mega_proto_003_uart_avr/src/main.c
@@ -5,12 +5,7 @@
 
 int main(void) {
 
-  //char note[] = "\ndata";
   embeddedcli_init();
-  //data((uint8_t *)&note,sizeof(note));
-  (*func_ptr[0])();
-  hal_serial_UART0_send((uint8_t *)cmd_list[0],4);
-  embeddedcli_cmd_help();
 
   while (1) {} 
   return 0; 
